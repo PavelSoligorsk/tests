000fb8767578add58e4afae5afd5687047492ddd
PavelSoligorsk <pavelkobrin58@gmail.com>
2026-04-24 19:48:38 +0300

    c

--- database.py
+++ database.py
@@ -1,14 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-# Имя файла базы данных
-SQLALCHEMY_DATABASE_URL = "sqlite:///./tasks_database.db"
+# Railway сам подставит DATABASE_URL
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:12345@localhost/tests")
 
-# Параметр check_same_thread нужен только для SQLite
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
